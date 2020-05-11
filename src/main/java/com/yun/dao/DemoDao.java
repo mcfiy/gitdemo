7670941903bc93bad742ea9eb404eb4a7fdbd809
mcfiy <alpess@163.com>
2020-05-11 10:57:57 +0800

    dao finished

---
+++ src/main/java/com/yun/dao/DemoDao.java
@@ -0,0 +1,13 @@
+package com.yun.dao;
+
+/**
+ * @version: V1.0
+ * @description: TODO
+ * @author: mcfiy
+ * @date: 2020-05-11 10:54
+ **/
+public class DemoDao {
+    void test(){
+        System.out.println("begin dao");
+    }
+}
